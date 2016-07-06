669dfa6d597be99916c7c708a748b1f11c70982e
René Olivo <minet.ws@gmail.com>
2016-07-06 09:32:08 -0400

    set published to false on create article

--- common/models/article.js
+++ common/models/article.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = function(Article) {
+  Article.observe('before save', function beforeSave(context, next) {
+    if (context.isNewInstance) {
+      context.instance.published = false;
+    }
 
+    next();
+  });
 };
