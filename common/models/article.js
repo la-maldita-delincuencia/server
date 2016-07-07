fc8136c988e05f76a5d067fcfd2be8d250b1ea54
René Olivo <minet.ws@gmail.com>
2016-07-06 20:16:26 -0400

    only authenticated users can publish articles

--- common/models/article.js
+++ common/models/article.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var loopback = require('loopback');
+
 module.exports = function(Article) {
   Article.observe('before save', function beforeSave(context, next) {
-    if (context.isNewInstance) {
+    var accessToken = loopback.getCurrentContext().get('accessToken');
+
+    if (!accessToken && context.isNewInstance) {
       context.instance.published = false;
     }
 
